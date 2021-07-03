75e8ccd1f43ca3a1ae498261cd16f12759f9c6c9
Tabata Nobuya <59525219+ponta1106@users.noreply.github.com>
2021-07-03 12:30:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const ojisan = document.getElementById('ojisan');
+const wig = document.getElementById('wig');
+
+let y = 10;
+let count = 0;
+
+const countUp = ()=> {
+    wig.style.top = y + 'px';
+    setTimeout(countUp, 100);
+    y += 10;
+
+    if(y > 260){
+        y = 0;
+    }
+}
+
+countUp();
