4d5ee1e5b0436351c5c1881855edd1ab9e116b34
Tabata Nobuya <59525219+ponta1106@users.noreply.github.com>
2021-07-18 03:08:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,14 @@ let defaultTime = 30;
 let point = 0;
 let ojisanLeft = 120;
 
-  document.documentElement.addEventListener('touchstart', function (e) {
-  if (e.touches.length >= 2) {e.preventDefault();}
-  }, {passive: false});
-  /* ダブルタップによる拡大を禁止 */
-  var t = 0;
-  document.documentElement.addEventListener('touchend', function (e) {
-  var now = new Date().getTime();
-  if ((now - t) < 350){
-    e.preventDefault();
+let lastTouch = 0;
+document.addEventListener('touchend', event => {
+  const now = window.performance.now();
+  if (now - lastTouch <= 500) {
+    event.preventDefault();
   }
-  t = now;
-  }, false);
+  lastTouch = now;
+}, true);
 
 const countUp = ()=> {
 
